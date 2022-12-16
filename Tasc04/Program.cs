663a52fd9e0f9d0f60622387cd80398136ed6b5d
[AlexeyMaiatnikov] <heldinbox87@gmail.com>
2022-12-16 17:45:04 +0800

    add Tasc04

---
+++ Tasc04/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+using static System.Console;
+Clear();
+WriteLine("Введите число N: ");
+int Number = Convert.ToInt32(ReadLine());
+int Numbers = 1;
+if (Number < Numbers) Write("Недопустимое число");
+while (Numbers <= Number)
+{
+    if (Numbers % 2 == 0)
+    {
+        Write($"{Numbers}, ");
+    }
+    Numbers++;
+}
